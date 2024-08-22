ed90de47259d8c82e72ac0267dc3f4c337054573
RSV <srepenko@gmail.com>
2024-08-22 15:40:25 +0300

    1

--- components/nice_bust4/__init__.py
+++ components/nice_bust4/__init__.py
@@ -7,18 +7,18 @@ DEPENDENCIES = ["uart"]
 
 nice_bust4_ns = cg.esphome_ns.namespace("nice_bust4")
 NiceBusT4 = nice_bust4_ns.class_(
-#    "NiceBusT4", cg.Component, uart.UARTDevice
-    "NiceBusT4", uart.IDFUARTComponent
+    "NiceBusT4", cg.Component, uart.UARTDevice
+#    "NiceBusT4", uart.IDFUARTComponent
 )
 
 CONFIG_SCHEMA = (
     cv.Schema({cv.GenerateID(): cv.declare_id(NiceBusT4)})
     .extend(cv.COMPONENT_SCHEMA)
-    #.extend(uart.UART_IDFUARTCOMPONENT_SCHEMA)
+    .extend(uart.UART_IDFUARTCOMPONENT_SCHEMA)
 )
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    #await uart.register_uart_device(var, config)
+    await uart.register_uart_device(var, config)
