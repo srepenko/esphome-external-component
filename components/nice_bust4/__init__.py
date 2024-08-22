b2e7d4b1af58d976403d5e6439032573be51f2da
RSV <srepenko@gmail.com>
2024-08-22 14:56:06 +0300

    1

--- components/nice_bust4/__init__.py
+++ components/nice_bust4/__init__.py
@@ -3,7 +3,7 @@ import esphome.config_validation as cv
 from esphome.components import uart
 from esphome.const import CONF_ID
 
-#DEPENDENCIES = ["uart"]
+DEPENDENCIES = ["uart"]
 
 nice_bust4_ns = cg.esphome_ns.namespace("nice_bust4")
 NiceBusT4 = nice_bust4_ns.class_(
