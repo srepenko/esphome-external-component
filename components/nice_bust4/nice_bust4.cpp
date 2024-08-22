9b14c9270e99995d3e9315bccfa427ad272e4029
RSV <srepenko@gmail.com>
2024-08-22 15:46:01 +0300

    1

--- components/nice_bust4/nice_bust4.cpp
+++ components/nice_bust4/nice_bust4.cpp
@@ -7,6 +7,15 @@ namespace nice_bust4 {
 static const char *TAG = "nice_bust4.component";
 
 void NiceBusT4::setup() {
+    //this->load_settings(119200);
+    //uint32_t invert = 0;
+    //if (this->tx_pin_ != nullptr && this->tx_pin_->is_inverted())
+    // invert |= UART_SIGNAL_TXD_INV;
+    //if (this->rx_pin_ != nullptr && this->rx_pin_->is_inverted())
+    // invert |= UART_SIGNAL_RXD_INV;
+    //uart_set_line_inverse(this->uart_num_, invert);
+
+
     uint32_t baudrate;
     uint8_t master_tx_buf[2];
     master_tx_buf[0] = 0x55; // sync byte
