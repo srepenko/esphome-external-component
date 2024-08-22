80f84401617327ed74356d419c37519e94638952
RSV <srepenko@gmail.com>
2024-08-22 14:42:08 +0300

    1

--- components/nice_bust4/__init__.py
+++ components/nice_bust4/__init__.py
@@ -7,8 +7,7 @@ DEPENDENCIES = ["uart"]
 
 nice_bust4_ns = cg.esphome_ns.namespace("nice_bust4")
 NiceBusT4 = nice_bust4_ns.class_(
-#    "NiceBusT4", cg.Component, uart.UARTDevice
-    "NiceBusT4", cg.Component, uart.IDFUARTComponent
+    "NiceBusT4", cg.Component, uart.UARTDevice
 )
 
 CONFIG_SCHEMA = (
