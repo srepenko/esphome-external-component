6413e7987275d8edec3608b5b08ee3c641476c6e
RSV <srepenko@gmail.com>
2024-08-22 17:59:33 +0300

    1

--- components/nice_bust4/nice_bust4.cpp
+++ components/nice_bust4/nice_bust4.cpp
@@ -7,30 +7,6 @@ namespace nice_bust4 {
 static const char *TAG = "nice_bust4.component";
 
 void NiceBusT4::setup() {
-    //this->load_settings(119200);
-    //uint32_t invert = 0;
-    //if (this->tx_pin_ != nullptr && this->tx_pin_->is_inverted())
-    // invert |= UART_SIGNAL_TXD_INV;
-    //if (this->rx_pin_ != nullptr && this->rx_pin_->is_inverted())
-    // invert |= UART_SIGNAL_RXD_INV;
-    //uart_set_line_inverse(this->uart_num_, invert);
-
-
-    uint32_t baudrate;
-    uint8_t master_tx_buf[] = {0x55, 0x0D, 0x00, 0xFF, 0x00, 0x66, 0x08, 0x06, 0x97, 0x00, 0x04, 0x99, 0x00, 0x00, 0x9D, 0x0D};
-//    master_tx_buf[0] = 0x55; // sync byte
-//    master_tx_buf[1] = 0x77;
-    uint8_t dummy = 0;
-    uint8_t lin_uart_num = this->uart_num_;
-    uart_flush_input(lin_uart_num);
-    uart_get_baudrate(lin_uart_num, &baudrate);
-#define LIN_BREAK_BAUDRATE(BAUD) ((BAUD * 9) / 13)
-    uart_set_baudrate(lin_uart_num, LIN_BREAK_BAUDRATE(baudrate));
-    uart_write_bytes(lin_uart_num, (char *)&dummy, 1);              // send a zero byte.  This call must be blocking.
-    uart_wait_tx_done(lin_uart_num, 2);                             // shouldn't be necessary??
-    uart_wait_tx_done(lin_uart_num, 2);                             // add 2nd uart_wait_tx_done per https://esp32.com/viewtopic.php?p=98456#p98456
-    uart_set_baudrate(lin_uart_num, baudrate);                      // set baudrate back to normal after break is sent
-    uart_write_bytes(lin_uart_num, (char *)master_tx_buf, sizeof(master_tx_buf));
 }
 
 void NiceBusT4::loop() {
@@ -49,7 +25,32 @@ void NiceBusT4::loop() {
     //    this->IDFUARTComponent::peek_byte(&c);
     //    ESP_LOGCONFIG(TAG, "%02X", c);
     //} //while
+    if(millis()-this->last_update >1000) {
+        //this->load_settings(119200);
+        //uint32_t invert = 0;
+        //if (this->tx_pin_ != nullptr && this->tx_pin_->is_inverted())
+        // invert |= UART_SIGNAL_TXD_INV;
+        //if (this->rx_pin_ != nullptr && this->rx_pin_->is_inverted())
+        // invert |= UART_SIGNAL_RXD_INV;
+        //uart_set_line_inverse(this->uart_num_, invert);
 
+
+        uint32_t baudrate;
+        uint8_t master_tx_buf[] = {0x55, 0x0D, 0x00, 0xFF, 0x00, 0x66, 0x08, 0x06, 0x97, 0x00, 0x04, 0x99, 0x00, 0x00, 0x9D, 0x0D};
+    //    master_tx_buf[0] = 0x55; // sync byte
+    //    master_tx_buf[1] = 0x77;
+        uint8_t dummy = 0;
+        uint8_t lin_uart_num = this->uart_num_;
+        uart_flush_input(lin_uart_num);
+        uart_get_baudrate(lin_uart_num, &baudrate);
+        uart_set_baudrate(lin_uart_num, LIN_BREAK_BAUDRATE(baudrate));
+        uart_write_bytes(lin_uart_num, (char *)&dummy, 1);              // send a zero byte.  This call must be blocking.
+        uart_wait_tx_done(lin_uart_num, 2);                             // shouldn't be necessary??
+        uart_wait_tx_done(lin_uart_num, 2);                             // add 2nd uart_wait_tx_done per https://esp32.com/viewtopic.php?p=98456#p98456
+        uart_set_baudrate(lin_uart_num, baudrate);                      // set baudrate back to normal after break is sent
+        uart_write_bytes(lin_uart_num, (char *)master_tx_buf, sizeof(master_tx_buf));
+        
+    }
 
 }
 
