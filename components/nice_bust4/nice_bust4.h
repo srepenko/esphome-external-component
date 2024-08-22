8cdf6d1e4940cdf390fc506ab67e63a5978620ec
RSV <srepenko@gmail.com>
2024-08-22 14:15:33 +0300

    1

--- components/nice_bust4/nice_bust4.h
+++ components/nice_bust4/nice_bust4.h
@@ -2,12 +2,12 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/uart/uart.h"
-
+#include "esphome/components/uart/uart_component_esp_idf.h"
 
 namespace esphome {
 namespace nice_bust4 {
 
-class NiceBusT4 : public uart::UARTDevice, public Component {
+class NiceBusT4 : public uart::IDFUARTComponent, public Component {
   public:
     void setup() override;
     void loop() override;
