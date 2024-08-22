c2fa4039986d443820c8112faab715ecb32cbf5d
RSV <srepenko@gmail.com>
2024-08-22 21:30:21 +0300

    1

--- components/nice_bust4/nice_bust4.h
+++ components/nice_bust4/nice_bust4.h
@@ -7,7 +7,7 @@
 namespace esphome {
 namespace nice_bust4 {
 //#define LIN_BREAK_BAUDRATE(BAUD) ((BAUD * 9) / 13)
-#define LIN_BREAK_BAUDRATE(BAUD) ((BAUD * 9) / 10)
+#define LIN_BREAK_BAUDRATE(BAUD) ((BAUD * 9) / 15)
 //class NiceBusT4 : public Component, public uart::UARTDevice{
 class NiceBusT4 : public uart::IDFUARTComponent{
   public:
