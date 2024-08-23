07f83233551c852c8da2a2f81a94c3a81161cbf7
RSV <srepenko@gmail.com>
2024-08-23 10:00:47 +0300

    1

---
+++ components/nice_bust4/cover.py
@@ -0,0 +1,4 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import cover
+from esphome.const import CONF_ADDRESS, CONF_ID, CONF_UPDATE_INTERVAL, CONF_USE_ADDRESS
