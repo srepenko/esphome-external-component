3c80c4596f63ad723f8edf1d22bc705a11cf8328
RSV <srepenko@gmail.com>
2024-08-23 11:53:44 +0300

    1

--- components/nice_bust4/cover/cover.py
+++ components/nice_bust4/cover/cover.py
@@ -9,8 +9,7 @@ from .. import CONF_NICEBUST4_ID, NICEBUST4_COMPONENT_SCHEMA, nice_bust4_ns
 
 NiceBusT4Cover = nice_bust4_ns.class_('NiceBusT4Cover', cover.Cover, cg.Component)
 
-#NICE_COVER_SCHEMA 
-CONFIG_SCHEMA  = cover.COVER_SCHEMA.extend({
+NICE_COVER_SCHEMA  = cover.COVER_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(NiceBusT4Cover),
     cv.Optional(CONF_ADDRESS): cv.hex_uint16_t,
     cv.Optional(CONF_USE_ADDRESS): cv.hex_uint16_t,
