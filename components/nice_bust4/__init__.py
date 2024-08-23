989b65cf3c524634226359a9aa354c2b7803a835
RSV <srepenko@gmail.com>
2024-08-23 10:26:31 +0300

    1

--- components/nice_bust4/__init__.py
+++ components/nice_bust4/__init__.py
@@ -29,10 +29,10 @@ CONFIG_SCHEMA = cv.All(
 )
 
 
-def to_code(config):
+async to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config)
-    yield uart.register_uart_device(var, config)
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
 
 
 
