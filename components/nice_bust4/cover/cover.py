9019ab0798bc9c4a9506ce5fa549d4f4515ced0e
RSV <srepenko@gmail.com>
2024-08-23 11:50:14 +0300

    1

--- components/nice_bust4/cover/cover.py
+++ components/nice_bust4/cover/cover.py
@@ -9,14 +9,15 @@ from .. import CONF_NICEBUST4_ID, NICEBUST4_COMPONENT_SCHEMA, nice_bust4_ns
 
 NiceBusT4Cover = nice_bust4_ns.class_('NiceBusT4Cover', cover.Cover, cg.Component)
 
-NICE_COVER_SCHEMA = cover.COVER_SCHEMA.extend({
+#NICE_COVER_SCHEMA 
+CONFIG_SCHEMA  = cover.COVER_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(NiceBusT4Cover),
     cv.Optional(CONF_ADDRESS): cv.hex_uint16_t,
     cv.Optional(CONF_USE_ADDRESS): cv.hex_uint16_t,
     cv.Optional(CONF_UPDATE_INTERVAL): cv.positive_time_period_milliseconds,
 }).extend(cv.COMPONENT_SCHEMA)
 
-CONFIG_SCHEMA = NICEBUST4_COMPONENT_SCHEMA.extend(NICE_COVER_SCHEMA)
+#CONFIG_SCHEMA = NICEBUST4_COMPONENT_SCHEMA.extend(NICE_COVER_SCHEMA)
         
 async def to_code(config):
     paren = await cg.get_variable(config[CONF_NICEBUST4_ID])
