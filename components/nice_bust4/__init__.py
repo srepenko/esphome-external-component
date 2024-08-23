3e365fd40377f79bb1d23d1ca998aa832c25cd87
RSV <srepenko@gmail.com>
2024-08-23 10:25:00 +0300

    1

--- components/nice_bust4/__init__.py
+++ components/nice_bust4/__init__.py
@@ -22,14 +22,28 @@ NICEBUST4_COMPONENT_SCHEMA = cv.Schema(
     }
 )
 
-CONFIG_SCHEMA = (
+CONFIG_SCHEMA = cv.All(
     cv.Schema({cv.GenerateID(): cv.declare_id(NiceBusT4)})
-    .extend(cv.COMPONENT_SCHEMA)
-#    .extend(uart.UART_DEVICE_SCHEMA)
+#    .extend(cv.polling_component_schema("1000ms"))
+    .extend(uart.UART_DEVICE_SCHEMA)
 )
 
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    #await uart.register_uart_device(var, config)
+    yield cg.register_component(var, config)
+    yield uart.register_uart_device(var, config)
+
+
+
+#CONFIG_SCHEMA = (
+#    cv.Schema({cv.GenerateID(): cv.declare_id(NiceBusT4)})
+#    .extend(cv.COMPONENT_SCHEMA)
+##    .extend(uart.UART_DEVICE_SCHEMA)
+#)
+
+
+#async def to_code(config):
+#    var = cg.new_Pvariable(config[CONF_ID])
+#    await cg.register_component(var, config)
+#    #await uart.register_uart_device(var, config)
