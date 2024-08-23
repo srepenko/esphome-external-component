5b0bbbad0485521a7bb9a4176ea99bd0ab34a806
RSV <srepenko@gmail.com>
2024-08-23 15:47:34 +0300

    1

--- components/nice_bust4/nice_bust4.h
+++ components/nice_bust4/nice_bust4.h
@@ -32,6 +32,8 @@ class NiceBusT4 : public uart::IDFUARTComponent{
     uint8_t last_published_op_;
     float last_published_pos_;
 
+    void parse_status_packet (const std::vector<uint8_t> &data); // разбираем пакет статуса
+
     void handle_char_(uint8_t c);                                         // обработчик полученного байта
     bool validate_message_();                                         // функция проверки полученного сообщения
 
