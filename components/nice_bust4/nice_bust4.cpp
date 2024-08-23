5723e9fdf83fd1c8116a7f0fadbc3ecc1102e17c
RSV <srepenko@gmail.com>
2024-08-23 16:01:35 +0300

    1

--- components/nice_bust4/nice_bust4.cpp
+++ components/nice_bust4/nice_bust4.cpp
@@ -12,10 +12,28 @@ void NiceBusT4::setup() {
 }
 
 void NiceBusT4::loop() {
+    if ((millis() - this->last_update_) > 5000) {    // каждые 10 секунд   
+    // если привод не определился с первого раза, попробуем позже
+        std::vector<uint8_t> unknown = {0x55, 0x55};
+        if (this->init_ok == false) {
+          this->tx_buffer_.push(gen_inf_cmd(0x00, 0xff, FOR_ALL, WHO, GET, 0x00));
+          this->tx_buffer_.push(gen_inf_cmd(0x00, 0xff, FOR_ALL, PRD, GET, 0x00)); //запрос продукта
+        }        
+        else if (this->class_gate_ == 0x55) init_device((uint8_t)(this->to_addr >> 8), (uint8_t)(this->to_addr & 0xFF), 0x04);  
+        else if (this->manufacturer_ == unknown)  {
+         init_device((uint8_t)(this->to_addr >> 8), (uint8_t)(this->to_addr & 0xFF), 0x04);  
+        }
+        this->last_update_ = millis();
+    }  // if  каждую минуту
+
 
     // разрешаем отправку каждые 100 ms
     const uint32_t now = millis();
-
+    if (now - this->last_uart_byte_ > 100) {
+        this->ready_to_tx_ = true;
+        this->last_uart_byte_ = now;
+    }
+    
     uint8_t data[128];
     int length = 0;
     ESP_ERROR_CHECK(uart_get_buffered_data_len(this->uart_num_, (size_t*)&length));
@@ -29,6 +47,15 @@ void NiceBusT4::loop() {
         }
         return;
     }
+    /*
+    if (this->ready_to_tx_) {   // если можно отправлять
+      if (!this->tx_buffer_.empty()) {  // если есть что отправлять
+        this->send_array_cmd(this->tx_buffer_.front()); // отправляем первую команду в очереди
+        this->tx_buffer_.pop();
+        this->ready_to_tx_ = false;
+      }
+    }
+    */
 }
 
 void NiceBusT4::dump_config(){
