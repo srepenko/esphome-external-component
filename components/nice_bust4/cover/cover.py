6e4f6ca2f25d6fca6df739d19170cf4f8a2bff46
RSV <srepenko@gmail.com>
2024-08-23 10:43:26 +0300

    1

--- components/nice_bust4/cover/cover.py
+++ components/nice_bust4/cover/cover.py
@@ -9,7 +9,7 @@ Nice = nice_bust4_ns.class_('NiceBusT4', cover.Cover, cg.Component)
 
 CONFIG_SCHEMA = cover.INVERTER_NICEBUST4_SCHEMA.extend({
     cv.GenerateID(): cv.declare_id(Nice),
-   cv.Optional(CONF_ADDRESS): cv.hex_uint16_t,
+    cv.Optional(CONF_ADDRESS): cv.hex_uint16_t,
     cv.Optional(CONF_USE_ADDRESS): cv.hex_uint16_t,
     cv.Optional(CONF_UPDATE_INTERVAL): cv.positive_time_period_milliseconds,
 }).extend(cv.COMPONENT_SCHEMA)
