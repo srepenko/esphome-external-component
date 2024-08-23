17af526f4f4fe0b7ab8558c748cfcb770f3a6963
RSV <srepenko@gmail.com>
2024-08-23 16:46:35 +0300

    1

--- components/nice_bust4/nice_bust4.h
+++ components/nice_bust4/nice_bust4.h
@@ -323,7 +323,7 @@ class NiceBusT4 : public uart::IDFUARTComponent{
     void dump_config() override;
 
 
-    void gen_control_cmd_(const uint8_t control_cmd){
+      void gen_control_cmd_(const uint8_t control_cmd){
         return tx_buffer_.push(gen_control_cmd(control_cmd));
     }
   protected:
@@ -357,7 +357,7 @@ class NiceBusT4 : public uart::IDFUARTComponent{
     std::vector<uint8_t> gen_inf_cmd(const uint8_t to_addr1, const uint8_t to_addr2, const uint8_t whose, const uint8_t inf_cmd, const uint8_t run_cmd, const uint8_t next_data, const std::vector<uint8_t> &data, size_t len);	 // все поля
     std::vector<uint8_t> gen_inf_cmd(const uint8_t whose, const uint8_t inf_cmd, const uint8_t run_cmd) {return gen_inf_cmd((uint8_t)(this->to_addr >> 8), (uint8_t)(this->to_addr & 0xFF), whose, inf_cmd, run_cmd, 0x00, {0x00}, 0 );} // для команд без данных
     std::vector<uint8_t> gen_inf_cmd(const uint8_t whose, const uint8_t inf_cmd, const uint8_t run_cmd, const uint8_t next_data, std::vector<uint8_t> data){
-	    return control_cmdgen_inf_cmd((uint8_t)(this->to_addr >> 8), (uint8_t)(this->to_addr & 0xFF), whose, inf_cmd, run_cmd, next_data, data, data.size());} // для команд c данными
+	    return gen_inf_cmd((uint8_t)(this->to_addr >> 8), (uint8_t)(this->to_addr & 0xFF), whose, inf_cmd, run_cmd, next_data, data, data.size());} // для команд c данными
     std::vector<uint8_t> gen_inf_cmd(const uint8_t to_addr1, const uint8_t to_addr2, const uint8_t whose, const uint8_t inf_cmd, const uint8_t run_cmd, const uint8_t next_data){
 	    return gen_inf_cmd(to_addr1, to_addr2, whose, inf_cmd, run_cmd, next_data, {0x00}, 0);} // для команд с адресом и без данных 	
 
