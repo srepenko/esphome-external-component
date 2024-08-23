27ffc9f099e2780939bf089c6ec63bf123d16632
RSV <srepenko@gmail.com>
2024-08-23 09:46:07 +0300

    1

--- components/nice_bust4/nice_bust4.h
+++ components/nice_bust4/nice_bust4.h
@@ -16,7 +16,8 @@ class NiceBusT4 : public Component, public uart::UARTDevice{
     void dump_config() override;
   protected:
     uint32_t last_update;
-    uart::IDFUARTComponent uart_;
+    //uart::IDFUARTComponent 
+    uint8_t uart_;
 };
 
 
