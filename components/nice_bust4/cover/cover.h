fb6570a0a7cc7ef335e0ba9acd91feb0ae65bbdb
RSV <srepenko@gmail.com>
2024-08-23 15:18:24 +0300

    1

--- components/nice_bust4/cover/cover.h
+++ components/nice_bust4/cover/cover.h
@@ -19,13 +19,13 @@ using namespace esphome::cover;
 // создаю класс, наследую членов классов Component и Cover
 class NiceBusT4Cover : public Component, public Cover{
   public:
-
+    InverterSelect() {}
     void setup() override;
     void loop() override;
     void dump_config() override; // для вывода в лог информации об оборудовнии
     void set_parent(NiceBusT4 *const parent) { this->parent_ = parent; }
     cover::CoverTraits get_traits() override;
-    
+
   protected:
     void control(const cover::CoverCall &call) override;
     NiceBusT4 *parent_;
