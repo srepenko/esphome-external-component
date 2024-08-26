bf147ac0b68133621bfe8764a02a9c9b8b6e5f84
RSV <srepenko@gmail.com>
2024-08-26 08:20:27 +0300

    1

--- components/nice_bust4/nice_bust4.cpp
+++ components/nice_bust4/nice_bust4.cpp
@@ -38,11 +38,17 @@ void NiceBusT4::loop() {
    
     uint8_t data[128];
     int length = 0;
-    ESP_ERROR_CHECK(uart_get_buffered_data_len(this->uart_num_, (size_t*)&length));
-    if (length > 0) {
-        length = uart_read_bytes(this->uart_num_, data, length, 100);
-        std::string pretty_cmd1 = format_hex_pretty(data, length);
-        ESP_LOGI(TAG,  "Входящие данные: %S ", pretty_cmd1.c_str() );
+    while (this->available ()) {
+      uint8_t c = this->peek_byte();
+      this->handle_char_(c);
+      this->last_uart_byte_ = now;
+    }
+    
+//    ESP_ERROR_CHECK(uart_get_buffered_data_len(this->uart_num_, (size_t*)&length));
+//    if (length > 0) {
+//        length = uart_read_bytes(this->uart_num_, data, length, 100);
+//        std::string pretty_cmd1 = format_hex_pretty(data, length);
+//        ESP_LOGI(TAG,  "Входящие данные: %S ", pretty_cmd1.c_str() );
 //        for (int i=0; i < length; ++i) {
 //            uart_read_bytes(this->uart_num_, data, 1, 100);
 //            this->handle_char_(data[0]);
